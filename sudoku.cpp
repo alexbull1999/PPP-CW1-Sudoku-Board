75dc96994f9b63b334e7876cac51f15a48e2ee05
Alex Bull <alexander.bull24@imperial.ac.uk>
2024-10-22 09:59:09 +0100

    Reformatted sudoku.h to 80 letter character lines

--- sudoku.cpp
+++ sudoku.cpp
@@ -79,7 +79,8 @@ bool is_complete(char board[9][9])
 
 	for (int row_num =0 ; row_num < 9; row_num++)
 	{
-		for (int col_num = 0 ; col_num < 9; col_num++) //Nested for loop required to iterate through columns 1-9 of row 0, before moving to row 1
+		for (int col_num = 0 ; col_num < 9; col_num++) 
+			//Nested for loop required to iterate through columns 1-9 of row 0, before moving to row 1
 		{
 			if (board[row_num][col_num] < '1' || board[row_num][col_num] > '9') //Using the ASCII values to check
 			{
@@ -89,10 +90,10 @@ bool is_complete(char board[9][9])
 		}
 	}
 
-	return true; //only triggered if all rows and columns have been iterated through without an empty space or incorrect character.
+	return true; //only triggered if all rows and columns iterated through without an empty space/incorrect character
 }
 
-/* internal helper function for make_move function to check if co-ords given are permissible (i.e. on the sudoku board) */
+/* internal helper function for make_move function to check if co-ords are permissible (i.e. on the sudoku board) */
 bool check_coords(const char position[2])
 {
 	if (position[0] < 'A' || position[0] > 'I') /* Checking if row co-ordinate is between A-I */
@@ -127,17 +128,17 @@ bool check_digit_placement(const char position[2], char digit, char board[9][9])
 {
 	char row_index = position[0];
 	char col_index = position[1];
-	int row_num = static_cast<int>(row_index) - 'A'; //Using ASCII values to rebase int to 0-8 corresponding to row index of A-I
-	int col_num = static_cast<int>(col_index) - '1'; //Using ASCII values to rebase int to 0-8 corresponding to col index of 1-9
+	int row_num = static_cast<int>(row_index) - 'A'; //Using ASCII values to rebase row_num to 0-8 corresponding to row index of A-I
+	int col_num = static_cast<int>(col_index) - '1'; //Using ASCII values to rebase col_num to 0-8 corresponding to col index of 1-9
 	
 	
 	if (board[row_num][col_num] >= '1' && board[row_num][col_num] <= '9')
 	{
-		return false; // if a digit is already in the cell (i.e. pre-given in the starting board), we do not want to overwrite it; even if the move could have theoretically been valid
+		return false; // if a digit is already in the cell (i.e. pre-given in the starting board), we do not want to overwrite it
 	}
 
 
-	for (int col_checker = 0; col_checker < 9; col_checker++) /* Iterate through all columns of desired row to input digit, to check if digit already appears and thus invalid placement */
+	for (int col_checker = 0; col_checker < 9; col_checker++) /* Iterate through all columns of desired row to input digit, to check if digit already appears and thus invalid */
 	{
 		if (board[row_num][col_checker] == digit)
 		{
@@ -145,7 +146,7 @@ bool check_digit_placement(const char position[2], char digit, char board[9][9])
 		}
 	}
 
-	for (int row_checker = 0; row_checker < 9; row_checker++) /* Iterate through all rows of desired column to input digit, to check if digit already appears and thus invalid placement */
+	for (int row_checker = 0; row_checker < 9; row_checker++) /* Iterate through all rows of desired column to input digit, to check if digit already appears and thus invalid */
 	{
 		if (board[row_checker][col_num] == digit)
 		{
@@ -153,7 +154,7 @@ bool check_digit_placement(const char position[2], char digit, char board[9][9])
 		}
 	}
 
-	/* We still need to check whether the digit is already present in the given 3x3 mini square, as if so that is also invalid placement */
+	/* We still need to check whether digit already present in the given 3x3 mini square, as if so that is also invalid */
 
 	int mini_sudoku_row_start;
 	int mini_sudoku_col_start;
@@ -184,7 +185,8 @@ bool check_digit_placement(const char position[2], char digit, char board[9][9])
 		mini_sudoku_col_start = 6;
 	}
 
-	for (int row_checker = mini_sudoku_row_start; row_checker < (mini_sudoku_row_start + 3); row_checker++) /* traversing through all squares in the mini 3x3 sudoku to make sure the digit doesn't already exist within it */
+	/* traversing through all squares in the mini 3x3 sudoku to make sure the digit doesn't already exist within it */
+	for (int row_checker = mini_sudoku_row_start; row_checker < (mini_sudoku_row_start + 3); row_checker++) 	
 	{
 		for (int col_checker = mini_sudoku_col_start; col_checker < (mini_sudoku_col_start + 3); col_checker++)
 		{
@@ -208,8 +210,8 @@ bool make_move(const char position[2], char digit, char board[9][9])
 {
 	char row_index = position[0];
 	char col_index = position[1];
-	int row_num = (static_cast<int>(row_index) - 'A'); //Using ASCII values to reset to 0 if row A
-	int col_num = (static_cast<int>(col_index) - '1'); //Using ASCII values to reset to 0 if col 1
+	int row_num = (static_cast<int>(row_index) - 'A'); //Using ASCII values to reset row_num to 0 if row A
+	int col_num = (static_cast<int>(col_index) - '1'); //Using ASCII values to reset col_num to 0 if col 1
 
 	if (check_coords(position) == true && check_digit(digit) == true && check_digit_placement(position, digit, board) == true)
 	{
@@ -235,7 +237,7 @@ bool save_board(const char filename[MAX_FILENAME_LENGTH], const char board[9][9]
 
 	for (int row_counter = 0; row_counter < 9; row_counter++)
 	{
-		for (int col_counter = 0; col_counter <= 9; col_counter++) /* Allowing col_counter to reach 9, so we know when to write a newline character to the file */
+		for (int col_counter = 0; col_counter <= 9; col_counter++) /* Allowing col_counter to reach 9, so we know when to write a newline character */
 		{
 			if (col_counter < 9)
 			{
@@ -252,19 +254,19 @@ bool save_board(const char filename[MAX_FILENAME_LENGTH], const char board[9][9]
 	return true;
 }
 
-/* Internal helper solve_board function with only one parameter, so it works with the calls of solve_board from main.cpp with only one parameter. 
- * Sets a recursion counter, that is then passed by reference into the proper solve_board function, called by this helper function. 
- * As this function calls the first "proper" solve_board function, and then returns the final result of the "proper" solve_board function, back to the 
- * main.cpp function, this function also allows us to start a timer using the <chrono> library, upon the first call of the program from main.cpp, and end that timer
- * once the "proper" solve_board has finished executing and returned true or false. This is an additional means of measuring sudoku board complexity. */
+/* Internal helper solve_board function with only one parameter, so it works with calls of solve_board from main.cpp with only one parameter. 
+ * Sets a recursion counter, that is passed by reference into the proper solve_board function, called by this helper function. 
+ * As this function calls the first "proper" solve_board function, and then returns the final result of it back to the 
+ * main.cpp function, this function also allows us to start a timer upon the first call of the program from main.cpp, and end that timer
+ * once it has finished executing and returned true or false. This is an additional means of measuring sudoku board complexity. */
 
 bool solve_board(char board[9][9])
 {
-	int recursion_counter = 0;
+	int recursion_counter = 0; // initialising recursion counter to 0 here, so every time a new solve_board (for a new Sudoku board) is called from main.cpp, counter is reset to 0 
 	auto start{chrono::high_resolution_clock::now()};
 	bool result = solve_board(board, recursion_counter);
 	auto end{chrono::high_resolution_clock::now()};
-	chrono::duration<double, std::milli> recursion_seconds{end-start};
+	chrono::duration<double, milli> recursion_seconds{end-start};
 	cout << "Total time taken to run solve_board function solve is: " << recursion_seconds.count() << " ms" << endl;
 	return result;
 }
@@ -272,7 +274,7 @@ bool solve_board(char board[9][9])
 
 /* solve_board main function */
 
-bool solve_board(char board[9][9], int &recursion_counter) //using a parameter passed by reference as a recursion counter 
+bool solve_board(char board[9][9], int &recursion_counter) //using the parameter passed by reference as a recursion counter 
 {
 
 	if (is_complete(board) == true) //check base case to start with, in case already solved.
@@ -285,13 +287,13 @@ bool solve_board(char board[9][9], int &recursion_counter) //using a parameter p
 	{
 		for (int col_counter = 0 ; col_counter < 9 ; col_counter++)
 		{
-			if (board[row_counter][col_counter] >= '1' && board[row_counter][col_counter] <= '9') /* If a square in the board is already complete, we can skip over it */
+			if (board[row_counter][col_counter] >= '1' && board[row_counter][col_counter] <= '9') /* If a square in the board is already complete, we skip it to find the next empty square */
 			{
 				continue;
 			}
 
 
-			for (char digit = '1'; digit <= '9'; digit++)
+			for (char digit = '1'; digit <= '9'; digit++) // iterate through all digits 1-9 for the next empty square, until a valid move can be found
 			{
 				char row_letter = static_cast<char>(row_counter) + 'A'; // Converting row_counter into co-ordinate letter form
 				char col_char = static_cast<char>(col_counter) + '1'; // Conveting col_counter into co-ordinate number form
@@ -301,12 +303,12 @@ bool solve_board(char board[9][9], int &recursion_counter) //using a parameter p
 
 				if (make_move(coords, digit, board) == true) // check if move is valid
 				{
-					recursion_counter++; //have to increment recursion counter here, before calling solve board in the next line if the make_move is valid. Otherwise it would be too late. 
-					if (solve_board(board, recursion_counter) == true) //recursively call solve board having incremented the recursion counter. The board passed into the new solve_board function call will have been updated with new move because make_move is true.
+					recursion_counter++; //have to increment recursion counter here, before calling solve board in the next line if make_move is valid. Otherwise would be too late. 
+					if (solve_board(board, recursion_counter) == true) //recursively call solve board; the board passed into the new solve_board function call will have been updated with new move because make_move is true.
 					{
 						return true; // cascade true back through the recursive calls
 					}
-					else //i.e. if solve_board cannot complete and return true, we need to reset the cell we are at; undoing the move previously made to allow the 'digit loop' to try to make a different move in the same co-ordinate with a new digit.
+					else //i.e. if solve_board cannot complete, we need to reset the cell we are at; undoing the move previously made to allow the 'digit loop' to try to make a different move.
 					{
 						board[row_counter][col_counter] = '.';
 					}
@@ -314,8 +316,8 @@ bool solve_board(char board[9][9], int &recursion_counter) //using a parameter p
 			}
 			
 			/* This means we have made it out of the for loop, with no digit representing a valid move. So we need
-			 * to return a false value from our current stack frame to the previous call of solve_board, so that it resets the
-			 * previous move made back to a null value. */
+			 * to return a false value from our current stack frame to the previous call of solve_board, so 
+			 * that it resets the previous move made back to a null value. */
 
 			return false; 		
 			
